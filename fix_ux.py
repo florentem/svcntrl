c163e10d7a9e00419e4df5a7d751387cb323b611
florentem <florentem@fedora>
2026-07-17 23:32:01 +0600

    Chore: remove temporary scripts and patch files

--- fix_ux.py
+++
@@ -1,8 +0,0 @@
-with open('src/main/java/com/svcntrl/core/UXManager.java', 'r') as f:
-    c = f.read()
-    
-c = c.replace('player.getBlockPos().getSquaredDistance(net.minecraft.util.math.BlockPos.ofFloored(project.getCenter())) > 16384',
-              'player.getBlockPos().getSquaredDistance(new net.minecraft.util.math.BlockPos((project.getMin().getX() + project.getMax().getX()) / 2, (project.getMin().getY() + project.getMax().getY()) / 2, (project.getMin().getZ() + project.getMax().getZ()) / 2)) > 16384')
-
-with open('src/main/java/com/svcntrl/core/UXManager.java', 'w') as f:
-    f.write(c)
