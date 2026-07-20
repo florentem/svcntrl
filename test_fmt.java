8d9d826a687044c2a6b2974cff77e1b0d115a985
florentem <florentem@fedora>
2026-07-20 20:21:19 +0600

    Remove accidentally committed junk files

--- test_fmt.java
+++
@@ -1,7 +0,0 @@
-public class test_fmt {
-    public static void main(String[] args) {
-        int maxVol = 5000000;
-        long volume = 80443475L;
-        System.out.println(String.format("Project area too large! Maximum volume is %s blocks, but you selected %s blocks.", maxVol, volume));
-    }
-}
